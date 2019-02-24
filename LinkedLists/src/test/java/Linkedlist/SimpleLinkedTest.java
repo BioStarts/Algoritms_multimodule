70c57ae3887cc7034261d1d3e1e60a849badd006
BioStarts <ipyo.ipa@gmail.com>
2019-02-24 21:01:21 +0300

    added Unit test for SimpleLinkedTest

--- LinkedLists/src/test/java/Linkedlist/SimpleLinkedTest.java
+++ LinkedLists/src/test/java/Linkedlist/SimpleLinkedTest.java
@@ -1,10 +1,51 @@
 package Linkedlist;
 
+import org.hamcrest.core.Is;
+import org.junit.Assert;
 import org.junit.Test;
+
 
 public class SimpleLinkedTest {
 
-    /*@Test
-    public void */
+    @Test
+    public void insert_test(){
+        SimpleLinkedListImpl<Integer> simpleLinkedList = new SimpleLinkedListImpl<>();
+
+        simpleLinkedList.insert(1);
+        simpleLinkedList.insert(2);
+        simpleLinkedList.insert(3);
+
+        Assert.assertThat(simpleLinkedList.currentSize, Is.is(3));
+        Assert.assertThat(simpleLinkedList.getFirstElement(), Is.is(3));
+
+    }
+
+    @Test
+    public void remove_test(){
+        SimpleLinkedListImpl<Integer> simpleLinkedList = new SimpleLinkedListImpl<>();
+
+        simpleLinkedList.insert(1);
+        simpleLinkedList.insert(2);
+        simpleLinkedList.insert(3);
+        simpleLinkedList.remove();
+
+        Assert.assertThat(simpleLinkedList.currentSize, Is.is(2));
+        Assert.assertThat(simpleLinkedList.getFirstElement(), Is.is(2));
+
+    }
+
+    @Test
+    public void remove_any_test(){
+        SimpleLinkedListImpl<Integer> simpleLinkedList = new SimpleLinkedListImpl<>();
+
+        simpleLinkedList.insert(1);
+        simpleLinkedList.insert(2);
+        simpleLinkedList.insert(3);
+        simpleLinkedList.remove(2);
+
+        Assert.assertThat(simpleLinkedList.currentSize, Is.is(2));
+        Assert.assertThat(simpleLinkedList.getFirstElement(), Is.is(3));
+
+    }
 
 }
