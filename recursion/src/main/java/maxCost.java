db89da5e791bd78729676d507563c1b75962bab2
vstartsev <vstartsev@amulex.ru>
2019-03-04 18:04:08 +0300

    /

--- recursion/src/main/java/maxCost.java
+++ recursion/src/main/java/maxCost.java
@@ -3,18 +3,18 @@ import java.util.ListIterator;
 
 public class maxCost {
 
-    static final int MAX_WEIGHT = 3;
+    static final int MAX_WEIGHT = 5;
 
     static int COUNT_STUFF;
 
-    static ArrayList<stuff> maxNext = new ArrayList();
-
+    static ArrayList maxNext = new ArrayList();
+    static int nRot = 0;
 
     public static void main(String[] args) {
 
         stuff lol = new stuff("Зонт", 1, 5); //создаем вещи
         stuff lol2 = new stuff("Часы", 2, 4);
-        stuff lol3 = new stuff("Шарф", 2, 7);
+        stuff lol3 = new stuff("Шарф", 1, 2);
         stuff lol4 = new stuff("Кольцо", 2, 3);
 
         stuff[] all = new stuff[4]; // добавляем в массив
@@ -32,29 +32,36 @@ public class maxCost {
         }
 
         System.out.println("Самый дорогой набор вещей: ");
-
-
-        goodSet(set);
-
-        display(maxNext);
+        display(goodSet(set));
 
     }
 
-
-
-    public static void goodSet(ArrayList allSet) {
+    public static ArrayList goodSet(ArrayList allSet) {
 
         if (setWeight(allSet) > MAX_WEIGHT) {
-            rotate(allSet); // если все вещи не вмещаеются в рюкзак начинаем рекурсию по поиску самого дорого набора
+            return rotate(allSet); // если все вещи не вмещаеются в рюкзак начинаем рекурсию по поиску самого дорого набора
         }
 
-        else maxNext.addAll(allSet); // в обратном случае сразу складываем все вещи в рюкзак
+        else return allSet;
     }
 
+    private static ArrayList rotate(ArrayList nextSet) { // рекурсивный метод для поиска из n вещей (n-1) набора самых дорогих вещей
 
-    private static void rotate(ArrayList nextSet) { // рекурсивный метод для перебора всех вариантов наборов
+        if ((nRot == COUNT_STUFF) && (maxNext != null)) { //выходим из рекурсии когда количесвто обходов равно количесвту вещей
+            return maxNext;
+        }
 
-        if ((setWeight(nextSet) <= MAX_WEIGHT) & (setCost(nextSet) > setCost(maxNext))) { //в обратном случае проверям дороже ли этот набор текущего самого дорогого и если да перезаписываем
+        ArrayList copyNext = new ArrayList(); //создаем копию для последующего сдвига и передачи в рекурсию
+        copyNext.addAll(nextSet);
+
+        nRot = nRot + 1; // считаем количесво рекурсивных обходов
+
+        nextSet.remove(0);//отсекаем первый элемент
+
+        if (setWeight(nextSet) > MAX_WEIGHT) { //если вес текущего набора превышает допустимый делаем смещение и передаем в рекурсию
+            shiftFirstByLast(copyNext);
+            return rotate(copyNext);
+        } else if (setCost(nextSet) > (setCost(maxNext))) { //в обратном случае проверям дороже ли этот набор текущего самого дорогого и если да перезаписываем
             // + делаем смещение и снова в рекурсию
 
             if (maxNext.size() == 0) { //если самый дорогой набор пустой складываем в него текущий (первый подходящий)
@@ -62,16 +69,19 @@ public class maxCost {
             }
 
             maxNext.clear(); //заменяем на более дорогой
-            maxNext.addAll(nextSet);}
+            maxNext.addAll(nextSet);
 
-        for (int i = 0; i < nextSet.size(); i++) {
-            ArrayList copyNext = new ArrayList(); //создаем копию для последующего сдвига и передачи в рекурсию
-            copyNext.addAll(nextSet);
-            copyNext.remove(i);
-            rotate(copyNext);
+            shiftFirstByLast(copyNext);
 
+            return rotate(copyNext);
         }
+        return rotate(copyNext);
+    }
 
+    private static void shiftFirstByLast(ArrayList anySet) { // делает сдвиг первого элемента на последний
+        stuff save0;
+        save0 = (stuff) anySet.remove(0);
+        anySet.add(anySet.size(), save0);
     }
 
 
